ce5c77648619ecdecdc441fc858513228f9a8fd2
acbca <idacbca@126.com>
2022-02-03 16:42:41 -0500

    1. Add system route forwarding configuration to gateway

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -3,6 +3,7 @@ package com.course.system.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/system")
 @RestController
 public class TestController {
 
