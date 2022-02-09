4106a7238ce2ef2a9851f996a600d110cb9fb652
acbca <idacbca@126.com>
2022-02-09 02:12:55 -0500

    1. added click event to Login button, jump to admin page.

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -48,7 +48,9 @@
                             <span class="lbl"> Remember Me</span>
                           </label>
 
-                          <button type="button" class="width-35 pull-right btn btn-sm btn-primary">
+                          <button type="button"
+                                  class="width-35 pull-right btn btn-sm btn-primary"
+                                  @click="login()">
                             <i class="ace-icon fa fa-key"></i>
                             <span class="bigger-110">Login</span>
                           </button>
@@ -74,6 +76,11 @@
 <script>
 $('body').attr('class', 'login-layout light-login');
 export default {
-  name: 'App'
+  name: 'login',
+  methods: {
+    login() {
+      this.$router.push("/admin")
+    }
+  }
 }
 </script>
